cfcecd42cf9bae816a1e95f94b2df1a17901a1d0
Even Vågen <evenvagen@gmail.com>
2022-12-27 11:56:55 +0100

    Finish section 3

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -3,22 +3,23 @@ import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #4" },
-    { name: "Friend #5" },
-    { name: "Friend #6" },
-    { name: "Friend #7" },
-    { name: "Friend #8" },
-    { name: "Friend #9" },
+    { name: "Friend #1", age: 26 },
+    { name: "Friend #2", age: 29 },
+    { name: "Friend #3", age: 20 },
+    { name: "Friend #4", age: 40 },
+    { name: "Friend #5", age: 50 },
+    { name: "Friend #6", age: 33 },
+    { name: "Friend #7", age: 90 },
+    { name: "Friend #8", age: 87 },
+    { name: "Friend #9", age: 37 },
   ];
 
   return (
     <FlatList
+      keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text>{item.name}</Text>;
+        return <Text>{item.name} - Age {item.age}</Text>;
       }}
     />
   );
