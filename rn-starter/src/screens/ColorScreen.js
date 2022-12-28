2d8028225ebf77aeb193c7d4cb7ddc704b8795b9
Even Vågen <evenvagen@gmail.com>
2022-12-28 14:10:41 +0100

    section 6 pt.2

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import {View, StyleSheet, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Button, FlatList} from 'react-native';
 
 const ColorScreen = () => {
+
+    const [colors, setColors] = useState([]);
+
     return <View>
-        <Button title="Add a color" />
+        <Button title="Add a color" onPress={() => {
+            setColors([...colors, randomColor()]);
+        }}/>
+        
+        <FlatList 
+            data={colors}
+            keyExtractor={(item) => item}
+            renderItem={({item}) => {
+                return <View style={{width: 100, height: 100, backgroundColor: item}}/>
+            }}
+        />
     </View>
+}
+
+const randomColor = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
 }
 
 const styles = StyleSheet.create({});
--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -11,23 +11,28 @@ const HomeScreen = ({ navigation }) => {
       />
 
        <Button
-        title="Go to list view"
+        title="Go to list demo"
         onPress={() => navigation.navigate('List')}
       />
 
        <Button
-        title="Go to image view"
+        title="Go to image demo"
         onPress={() => navigation.navigate('Image')}
       />
 
        <Button
-        title="Go to counter view"
+        title="Go to counter demo"
         onPress={() => navigation.navigate('Counter')}
       />
 
       <Button
-        title="Go to color view"
+        title="Go to color demo"
         onPress={() => navigation.navigate('Color')}
+      />
+
+      <Button
+        title="Go to square demo"
+        onPress={() => navigation.navigate('Square')}
       />
     
     </View>
