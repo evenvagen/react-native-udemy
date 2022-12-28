69d8fe8ce4f900ec4e275d810e39d2b50686fe44
Even Vågen <evenvagen@gmail.com>
2022-12-28 08:39:58 +0100

    section 5 finish

--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -1,8 +1,15 @@
 import React from 'react'; 
-import { View, Text, StyleSheet } from 'react-native'; 
+import { View, Text, StyleSheet } from 'react-native';
+import ImageDetail from '../components/ImageDetail' 
 
 const ImageScreen = () => { 
-    return <Text>Image screen</Text>
+    return (
+    <View>
+        <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} score={8} />
+        <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} score={9} />
+        <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} score={7} />
+    </View>
+        )
 };
 
 const styles = StyleSheet.create({});
