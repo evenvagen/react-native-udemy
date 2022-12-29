d46c205c2c59b3bae4cd5f6d3714ccde4524169e
Even Vågen <evenvagen@gmail.com>
2022-12-29 14:05:46 +0100

    section 6 reducer pt.1

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react'; 
+import React, { useReducer } from 'react'; 
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 const COLOR_INCREMENT = 15;
 
+const reducer = (state, action) => {
+
+};
+
 const SquareScreen = () => {
     
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
+    const [state, dispath] = useReducer(reducer, {red: 0, green: 0, blue: 0});
     
     return (
     <View>
-       <ColorCounter color="Red" onIncrease={() => setRed(red + COLOR_INCREMENT)} onDecrease={() => setRed(red - COLOR_INCREMENT)} />
-       <ColorCounter color="Blue" onIncrease={() => setBlue(blue + COLOR_INCREMENT)} onDecrease={() => setBlue(blue - COLOR_INCREMENT)} />
-       <ColorCounter color="Green" onIncrease={() => setGreen(green + COLOR_INCREMENT)} onDecrease={() => setGreen(green - COLOR_INCREMENT)} />
+       <ColorCounter color="Red" onIncrease={() => setColor('red', COLOR_INCREMENT)} onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)} />
+       <ColorCounter color="Blue" onIncrease={() => setColor('blue', COLOR_INCREMENT)} onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}  />
+       <ColorCounter color="Green" onIncrease={() => setColor('green', COLOR_INCREMENT)} onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}  />
        <View style={{ height:150, width: 150, backgroundColor: `rgb(${red},${green},${blue})` }} />
     </View>
         )
